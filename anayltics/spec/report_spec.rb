8648631f9e1b52b85878632a411ba799a532901b
ConnerLeo <cl3093@gmail.com>
2014-12-07 17:59:29 -0500

    Report spec and edit to game spec

--- anayltics/spec/report_spec.rb
+++ anayltics/spec/report_spec.rb
@@ -6,23 +6,23 @@ describe Report do
   test_obj = Report.new()
 
   it 'checks calling analyze() passing an invalid file path will result in error' do
-    expect{analyze(random/invalid/path)}.to raise_error
+    expect{analyze("random/invalid/path")}.to raise_error
   end
   
   it 'checks calling generate_all() passing an invalid file path will result in error' do
-    expect{generate_all(random/invalid/path)}.to raise_error
+    expect{generate_all("random/invalid/path")}.to raise_error
   end
   
   it 'checks calling get_win_rates() passing an invalid file path will result in error' do
-    expect{get_win_rates(random/invalid/path)}.to raise_error
+    expect{get_win_rates("random/invalid/path")}.to raise_error
   end
   
   it 'checks calling get_win_turn() passing an invalid file path will result in error' do
-    expect{get_win_turn(random/invalid/path)}.to raise_error
+    expect{get_win_turn("random/invalid/path")}.to raise_error
   end
   
   it 'checks calling get_win_damage() passing an invalid file path will result in error' do
-    expect{get_win_damage(random/invalid/path)}.to raise_error
+    expect{get_win_damage("random/invalid/path")}.to raise_error
   end
 
   
